cad1f8958af805486c10c8f00f3932e86b1b3214
root <root@jordaveLaptop.(none)>
2012-04-09 23:24:43 -0500

    gii tool

--- protected/modules/crmColaboracion/views/calendario/index.php
+++ protected/modules/crmColaboracion/views/calendario/index.php
@@ -1,14 +1,43 @@
+<form name="parametros" id="parametros" method="POST" enctype="multipart/form-data">
+
 <table>
     <tr>
         <td>Mes:</td>
-        <td></td>
+        <td>
+            <select name="mes" id="mes">
+                <option value="1">Enero</option>
+                <option value="2">Febrero</option>
+                <option value="3">Marzo</option>
+                <option value="4">Abril</option>
+                <option value="5">Mayo</option>
+                <option value="6">Junio</option>
+                <option value="7">Julio</option>
+                <option value="8">Agosto</option>
+                <option value="9">Septiembre</option>
+                <option value="10">Octubre</option>
+                <option value="11">Noviembre</option>
+                <option value="12">Diciembre</option>                
+            </select>
+            
+            
+        </td>
     </tr>
     <tr>
         <td>A&ntilde;o:</td>
-        <td></td>
+        <td>
+            <select name="anio" id="anio">
+                <?php
+                                      
+                    for( $anio = date('Y') -2;$anio<=date('Y')+2;$anio++)
+                    {
+                        echo '<option value="'.$anio.'">'.$anio.'</option>';
+                    }
+                ?>
+            </select>
+        </td>
     </tr>
 </table>
-
+</form>
 <?php
 /**
  *Construir el calendario
